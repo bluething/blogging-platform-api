31e290ee6432e19151d9d6416e0ff0e91f1ccfb4
Habib Ali Machpud <habib.machpud@gmail.com>
2025-05-18 00:03:18 +0700

    Init

---
+++ src/test/java/io/github/bluething/playground/java/bloggingplatformapi/BloggingPlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.java.bloggingplatformapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingPlatformApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
